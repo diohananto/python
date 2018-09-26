4d98dedb61c2f6174ba08013fc41f8fd59433298
diohananto <diohananto98@gmail.com>
2018-09-27 01:23:48 +0700

    program menentukan bilangan genap atau ganjil

--- program04.py
+++ program04.py
@@ -1,4 +1,4 @@
-#modulus, program untuk menentukan bilangan genap atau ganjil
+#modulus, program untuk menentukan angka genap atau ganjil
 print "bilangan genap dan ganjil"
 sisi=int(input("masukan angka ="))
 if sisi%2==0:
