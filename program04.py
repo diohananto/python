1bb48f1d8a68ac868c1608310923456b5cc290a9
diohananto <diohananto98@gmail.com>
2018-09-27 01:19:00 +0700

    program menentukan bilangan genap atau ganjil

--- program04.py
+++ program04.py
@@ -1,5 +1,6 @@
-print "percabangan | bilangan genap dan ganjil"
-sisi=input(" masukan angka =")
+#modulus, program untuk menentukan bilangan genap atau ganjil
+print "bilangan genap dan ganjil"
+sisi=input("masukan angka =")
 if sisi%2==0:
     print"bilangan genap"
 else:
