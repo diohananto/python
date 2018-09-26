b01b5981a7019ef7ee448e767ed60b28a66b5b98
diohananto <diohananto98@gmail.com>
2018-09-27 01:17:30 +0700

    program penjumlahan angka

---
+++ program05.py
@@ -0,0 +1,10 @@
+#program jumlah angka sederhana
+print"PRAKTIKUM PEMROGRAMAN KOMPUTER"
+print "Ini adalah program yang ditulis dengan Bahasa Python" 
+print "Berikut contoh program penambahan"
+a = 2
+b = 3
+hasil = a + b
+print "hasil = a + b"
+print "hasil = %d + %d" % (a,b) 
+print "hasil = %d" % (hasil)
